ee639cfb4d67fd47e3747912cce098db459779c4
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-10-13 20:19:33 +1000

    Import code from Maz

---
+++ routes/twitter.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var Twitter = require('twitter');
+const Sentiment = require('natural').SentimentAnalyzer;
+const stemmer = require('natural').PorterStemmer;
+const analyzer = new Sentiment("English",stemmer,'afinn');
+const natural = require("natural");
+const tokenizer = new natural.WordTokenizer();
+require('dotenv').config();
+
+
+/* GET */
+router.get('/', function(req, res, next) {
+    var client = new Twitter({
+        consumer_key: process.env.api_key,
+        consumer_secret: process.env.api_secret_key,
+        access_token_key: process.env.access_token,
+        access_token_secret: process.env.access_token_secret
+      });
+      client.get('search/tweets', { q: 'BLM',count: 100,lang:  'en' }, function (error, tweets, response) {
+        console.log(tweets);
+        var str = ""
+        var i = 1;
+        var total = 0;
+        var array= [];
+        tweets.statuses.forEach(status => {
+          var sent = analyzer.getSentiment(tokenizer.tokenize(status.text));
+          array.push({id:  status.id, text: status.text, score: sent})
+          i++;
+          if(Number.isNaN(sent)){
+            sent = 0;
+          }
+          total += sent;
+          console.log(sent);
+        });
+        
+        var totalScore = total/i;
+        var sentiment ="";
+        console.log(total)
+        console.log(i)
+        console.log(totalScore)
+        if(totalScore<0){
+          sentiment = "Negative";
+        }else {
+          sentiment = "Possitive";
+        }
+        res.json({ array,totalScore,sentiment });
+      });
+});
+
+module.exports = router;
